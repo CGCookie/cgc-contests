45d588ef72367e9c92a1a8f3cbf7371a352a6ba0
Bearded Avenger <email@nickhaskins.com>
2014-10-09 11:41:38 -0500

    add body class based on activated theme

--- public/class-cgc-contests.php
+++ public/class-cgc-contests.php
@@ -231,7 +231,12 @@ class CGC_Contests {
 
 
 	    if ( cgc_contest_meta( get_the_ID(), '_cgc_contest_page' ) )
-			$classes[] = 'cgc-contest-page';
+		$theme 		= wp_get_theme();
+		$get_name  	=  strtolower( $theme->get( 'Name' ) );
+		$name 		= str_replace(' ', '-', $get_name );
+
+		$classes[] = 'cgc-contest-page';
+		$classes[] = 'contest-page-on-'.$name;
 
 		return $classes;
 	}
