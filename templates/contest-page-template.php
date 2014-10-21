57aa9f4fdf466e358aedb6e38668f979b33c4423
Bearded Avenger <email@nickhaskins.com>
2014-10-21 09:50:26 -0500

    more fixing meta from cmb2 move

--- templates/contest-page-template.php
+++ templates/contest-page-template.php
@@ -4,6 +4,8 @@
 
 if ( have_posts() ) : while ( have_posts() ) : the_post();
 
+	// fix backwards compatibility with the move to cmb2
+
 	// get meta
 	$rules 			= cgc_contest_meta( get_the_ID(),'_cgc_contest_rules' );
 	$sponsors 		= cgc_contest_meta( get_the_ID(), '_cgc_contest_sponsors' );
@@ -16,7 +18,7 @@ if ( have_posts() ) : while ( have_posts() ) : the_post();
 	$faqs  			= cgc_contest_meta( get_the_ID(),'_cgc_contest_faq' );
 	$subtitle       = cgc_contest_meta( get_the_ID(), '_cgc_contest_subtitle');
 	$banner_src   	= get_post_meta( get_the_ID(), '_cgc_contest_banner', true );
-	$banner     	= wp_get_attachment_url($banner_src,'full');
+	//$banner     	= wp_get_attachment_image_src($banner_src,'full');
 	$accent_color   = cgc_contest_meta( get_the_ID(), '_cgc_contest_color' );
 
 	$gform			= get_post_meta( get_the_ID(), '_cgc_contest_gform_id', true );
@@ -28,12 +30,13 @@ if ( have_posts() ) : while ( have_posts() ) : the_post();
 
 	$entries_page  = $all_entries ? get_permalink($all_entries[0]) : null;
 
+
 ?>
 <style>
 .page-id-<?php echo get_the_ID();?> .cgc-contest-rules li:before,
 .page-id-<?php echo get_the_ID();?>.cgc-contest-page .styled-list li:before,
-.page-id-<?php echo get_the_ID();?>.cgc-contest-page .page-content a:not(.button){color:<?php echo $accent_color[0];?>;}
-.page-id-<?php echo get_the_ID();?>.cgc-contest-page .page-content .button{background:<?php echo $accent_color[0];?>;}
+.page-id-<?php echo get_the_ID();?>.cgc-contest-page .page-content a:not(.button){color:<?php echo esc_html( $accent_color[0] );?>;}
+.page-id-<?php echo get_the_ID();?>.cgc-contest-page .page-content .button{background:<?php echo esc_html( $accent_color[0] );?>;}
 
 </style>
 <div class="page-content">
@@ -42,10 +45,10 @@ if ( have_posts() ) : while ( have_posts() ) : the_post();
 		<div class="cgc-contest-header-inner">
 			<?php the_title('<h1>','</h1>');?>
 			<?php if ( $subtitle ) { ?>
-				<h2><?php echo $subtitle[0];?></h2>
+				<h2><?php echo esc_html( $subtitle[0] );?></h2>
 			<?php } ?>
 		</div>
-		<div class="cgc-contest-header-img" style="background-image:url('<?php echo $banner;?>');"></div>
+		<div class="cgc-contest-header-img" style="background-image:url('<?php echo esc_url( $banner_src );?>');"></div>
 	</section>
 	<div class="page-content-inner">
 
@@ -63,9 +66,11 @@ if ( have_posts() ) : while ( have_posts() ) : the_post();
 						<?php
 							if ( $rules ):
 
-								foreach ( $rules as $rule ):
+								foreach ( (array) $rules as $key => $rule ):
 
-									printf('<li>%s</li>', $rule);
+									$getrule = isset( $rule ) ? $rule : null;
+
+									printf('<li>%s</li>', esc_html( $getrule[0] ) );
 
 								endforeach;
 
@@ -80,17 +85,19 @@ if ( have_posts() ) : while ( have_posts() ) : the_post();
 			<div class="cgc-contest-inner cgc-contest-back">
 				<h4>The sponsors behind the challenge</h4>
 
-				<ul class="cgc-contest-sponsor-logos cgc-contest-<?php echo $count_sponsors;?>-sponsors">
+				<ul class="cgc-contest-sponsor-logos cgc-contest-<?php echo absint( $count_sponsors );?>-sponsors">
 					<?php
 						if ( $sponsors ):
 
-							foreach ( $sponsors as $sponsor ):
+							foreach ( (array) $sponsors as $key => $sponsor ):
 
-								$getimg = $sponsor['img'];
+								$getimg = $img = $link = '';
+
+								$getimg = isset( $sponsor['img'] ) ? $sponsor['img'] : null;
 								$img    = wp_get_attachment_image($getimg,'full');
-								$link   = $sponsor['link'];
+								$link   = isset( $sponsor['link'] ) ? $sponsor['img'] : null;
 
-								printf('<li><a href="%s">%s</a></li>', $link, $img);
+								printf('<li><a href="%s">%s</a></li>', esc_url( $link ), esc_url( $img ) );
 
 							endforeach;
 
@@ -106,7 +113,7 @@ if ( have_posts() ) : while ( have_posts() ) : the_post();
 					<?php
 						if ( $awards ):
 
-							foreach ( $awards as $award ):
+							foreach ( (array) $awards as $key => $award ):
 
 								printf('<li><div class="cgc-contest-award-inner">%s</div></li>', wpautop($award) );
 
@@ -116,12 +123,12 @@ if ( have_posts() ) : while ( have_posts() ) : the_post();
 
 					?>
 				</ul>
-				<ul class="cgc-contest-awards cgc-contest-extra-awards cgc-contest-<?php echo $count_extra_awards;?>-extra-awards">
+				<ul class="cgc-contest-awards cgc-contest-extra-awards cgc-contest-<?php echo absint( $count_extra_awards );?>-extra-awards">
 					<?php
 
 						if ( $extra_awards ):
 
-							foreach ( $extra_awards as $extra_award ):
+							foreach ( (array) $extra_awards as $key => $extra_award ):
 
 								printf('<li><div class="cgc-contest-award-inner">%s</div></li>', wpautop($extra_award) );
 
@@ -135,14 +142,14 @@ if ( have_posts() ) : while ( have_posts() ) : the_post();
 
 		<div class="cgc-contest-cta-wrap">
 			<a href="cgc-contest-form" data-reveal-id="cgc-contest-form" class="button cgc-open-contest-modal">Submit your entry</a>
-			<p>Entries must be recieved by <?php echo $expires;?></p>
+			<p>Entries must be recieved by <?php echo esc_html( $expires );?></p>
 		</div>
 
 		<section class="cgc-contest-recent-entries">
 			<div class="cgc-contest-inner cgc-contest-back">
 				<h3>Recent Challenge Entries</h3>
 				<a class="cgc-contest-all-entries" href="<?php echo $entries_page;?>">View all entries <i class="icon icon-caret-right"></i></a>
-				<?php echo do_shortcode('[cgc_contest id="'.$gform.'" position="'.$gfield.'" limit="9" ]');?>
+				<?php echo do_shortcode('[cgc_contest id="'.absint( $gform ).'" position="'.absint( $gfield ).'" limit="9" ]');?>
 			</div>
 		</section>
 
@@ -153,13 +160,15 @@ if ( have_posts() ) : while ( have_posts() ) : the_post();
 					<?php
 						if ( $faqs ):
 
-							foreach ( $faqs as $faq ):
+							foreach ( (array) $faqs as $key => $faq ):
 
-								$question = $faq['question'];
-								$answer   = $faq['answer'];
+								$question = $answer = '';
+
+								$question = isset( $faq['question'] ) ? $faq['question'] : null;
+								$answer   = isset( $faq['answer'] ) ? $faq['answer'] : null;
 
 								?><li>
-									<h5 class="cgc-contest-faq-question"><?php echo $question;?></h5>
+									<h5 class="cgc-contest-faq-question"><?php echo esc_html( $question );?></h5>
 									<div class="cgc-contest-faw-answer"><?php echo wpautop($answer);?></div>
 								</li><?php
 
@@ -176,7 +185,7 @@ if ( have_posts() ) : while ( have_posts() ) : the_post();
 	<!-- Contest Modal -->
 	<div id="cgc-contest-form" class="reveal-modal" data-reveal>
 		<h3 class="reveal-modal-header">Entry Submission</h3>
-		<?php gravity_form($gform, false, true, false, null, true);?>
+		<?php gravity_form( absint( $gform ), false, true, false, null, true);?>
 		<a class="close-reveal-modal">&#215;</a>
 	</div>
 </div>
